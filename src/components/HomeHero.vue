ecb372610777a4f39ce2030ba3b213f98028ad88
exanwar <exanwar@gmail.com>
2021-05-30 02:21:11 +0600

    Responsive done with some issues

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,108 @@
+<template>
+  <!-- Hero Section -->
+  <div
+    class="
+      lg:max-w-7xl
+      md:max-w-3xl
+      mx-auto
+      flex
+      lg:flex-row
+      flex-col
+      justify-center
+      items-center
+      dark:text-gray-200
+    "
+  >
+    <div class="lg:hidden flex justify-center">
+      <img
+        src="https://iokit.netlify.app/assets/images/illustration/Payment%20Processing.svg"
+        class="h-full w-6/12"
+      />
+    </div>
+    <div class="lg:flex-1 lg:pl-52 flex flex-col justify-center p-8">
+      <h1
+        class="
+          text-3xl
+          sm:text-4xl
+          md:text-5xl
+          lg:text-7xl
+          font-bold
+          text-gray-700
+          dark:text-gray-200
+          font-pattaya
+          tracking-wider
+          lg:tracking-wide
+          leading-tight
+        "
+      >
+        Grow Your Search for your stunning great
+      </h1>
+      <div
+        class="
+          mt-10
+          pr-16
+          text-gray-900
+          dark:text-gray-200
+          text-md
+          leading-5
+          tracking-wide
+          font-medium
+        "
+      >
+        <p class="font-nunito">
+          iokit gives you the freedom to create your saas, startup or digital
+          agency platform easy.
+        </p>
+        <p class="font-nunito mt-2">lets start with us</p>
+      </div>
+      <div class="mt-10">
+        <button
+          class="
+            flex
+            items-center
+            bg-blue-500
+            rounded-lg
+            px-4
+            py-2.5
+            text-gray-100
+            font-bold
+            hover:bg-blue-900
+          "
+        >
+          <span class="tracking-tight font-sm">Get Started </span>
+          <span
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              /></svg
+          ></span>
+        </button>
+      </div>
+    </div>
+    <div class="lg:flex lg:flex-1 lg:py-10 hidden">
+      <img
+        src="https://iokit.netlify.app/assets/images/illustration/Payment%20Processing.svg"
+        class="h-full w-full"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/HomePartners.vue
@@ -0,0 +1,120 @@
+<template>
+  <div
+    class="
+      lg:max-w-7xl
+      md:max-w-4xl
+      sm:max-w-xl
+      mx-auto
+      pb-28
+      space-y-5
+      justify-center
+      items-center
+    "
+  >
+    <div class="flex flex-row justify-between items-center space-x-2">
+      <div class="flex flex-1 h-0.5 w-full bg-gray-400"></div>
+      <div class="flex flex-3">
+        <p
+          class="
+            font-bold font-xs
+            text-gray-600
+            font-pattaya
+            tracking-wider
+            dark:text-gray-300
+          "
+        >
+          Trusted by template creators across the world
+        </p>
+      </div>
+      <div class="flex flex-1 h-0.5 w-full bg-gray-400"></div>
+    </div>
+    <div class="">
+      <div class="flex flex-row justify-around items-center">
+        <img
+          src="https://iokit.netlify.app/assets/images/logos/client_logo2.svg"
+          class="
+            lg:w-28
+            w-12
+            h-10
+            transform
+            hover:scale-125
+            transition
+            duration-700
+          "
+        />
+        <img
+          src="https://iokit.netlify.app/assets/images/logos/client_logo4.svg"
+          class="
+            lg:w-28
+            md:w-20
+            sm:w-16
+            w-12
+            h-10
+            transform
+            hover:scale-125
+            transition
+            duration-700
+          "
+        />
+        <img
+          src="https://iokit.netlify.app/assets/images/logos/client_logo3.svg"
+          class="
+            lg:w-28
+            w-12
+            h-10
+            transform
+            hover:scale-125
+            transition
+            duration-700
+          "
+        />
+        <img
+          src="https://iokit.netlify.app/assets/images/logos/client_logo1.svg"
+          class="
+            lg:w-28
+            w-12
+            h-10
+            transform
+            hover:scale-125
+            transition
+            duration-700
+          "
+        />
+        <img
+          src="https://iokit.netlify.app/assets/images/logos/client_logo5.svg"
+          class="
+            lg:w-28
+            w-12
+            h-10
+            transform
+            hover:scale-125
+            transition
+            duration-700
+          "
+        />
+        <img
+          src="https://iokit.netlify.app/assets/images/logos/client_logo6.svg"
+          class="
+            lg:w-28
+            w-12
+            h-10
+            transform
+            hover:scale-125
+            transition
+            duration-700
+          "
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
